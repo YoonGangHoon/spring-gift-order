ba8e158ef6837002482e7d204d1b007b626cb592
YoonGangHoon <yghun021007@naver.com>
2025-07-28 22:26:39 +0900

    feat: 카카오 로그인 시 닉네임을 받을 수 있는 기능 추가

--- src/main/java/gift/dto/KakaoUserInfoResponseDto.java
+++ src/main/java/gift/dto/KakaoUserInfoResponseDto.java
@@ -1,4 +1,14 @@
 package gift.dto;
 
-public record KakaoUserInfoResponseDto(Long id) {
+public record KakaoUserInfoResponseDto(
+        Long id,
+        KakaoAccount kakao_account
+) {
+    public record KakaoAccount(
+            Profile profile
+    ) {
+        public record Profile(
+                String nickname
+        ) {}
+    }
 }
